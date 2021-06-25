3ce96d4ba4e167232c5863d80f0c40c7405a8bca
James Deen Odchigue De Belen <deen@alphasys.com.au>
2021-06-25 14:42:14 +0800

    Update React JS Crash 2021

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
+import { useState } from "react" 
+import { FaTrashAlt } from "react-icons/fa"
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+])
+
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    console.log(id)
+    setTasks(
+      tasks.map((task) => task.id === id ? 
+      { ...task, reminder: !task.reminder } 
+      : 
+      task
+      )
+    )
+  }
+
+
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? (
+        <Tasks 
+        tasks={tasks}
+        onDelete={deleteTask}
+        onToggle={toggleReminder} />
+      ) : (
+        'No Task To Show'
+      )}
     </div>
   );
 }
